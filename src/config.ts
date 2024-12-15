420a6019d0d6baa396f1ed498cbd2b396b898a4c
gamemasters-alex <153458604+gamemasters-alex@users.noreply.github.com>
2024-12-15 14:17:49 +0100

    update API url in config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const env = process.env.NODE_ENV;
 
 const config = {
-  apiUrl: env === 'development' ? 'http://localhost:5173' : 'https://showmaster-ggb4j.ondigitalocean.app',
+  apiUrl: env === 'development' ? 'http://localhost:5173' : 'https://clownfish-app-4c6ye.ondigitalocean.app',
 };
 
 export default config;
